6116677cdb55cb65943b199ed4fede6a398df61e
Claus Witt <claus@wittnezz.dk>
2016-11-10 09:04:09 +0100

    initial commit

---
+++ lib/omniauth/fliva.rb
@@ -0,0 +1,7 @@
+require "omniauth/fliva/version"
+
+module Omniauth
+  module Fliva
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/fliva/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Fliva
+    VERSION = "0.1.0"
+  end
+end
