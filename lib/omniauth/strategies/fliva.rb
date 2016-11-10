87abebec27e6ac77633e318badee81f7dc54c25f
Claus Witt <claus@wittnezz.dk>
2016-11-10 10:28:28 +0100

    fixes location of auth info

--- lib/omniauth/strategies/fliva.rb
+++ lib/omniauth/strategies/fliva.rb
@@ -19,7 +19,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/me.json').parsed
+        @raw_info ||= access_token.get('/me.json').parsed
       end
 
       def callback_url
